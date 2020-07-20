59943f7719fddade4938a9fa43408cd6b6d9c6c6
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-07-20 07:43:51 +0300

    correction on the metrix elements of H - math doc

--- Code/Python/Diagonalization.py
+++ Code/Python/Diagonalization.py
@@ -1,28 +1,43 @@
-import numpy
+import numpy as np
 from numpy import random as rd
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
 
 
-def CreateMatrix02(n,e,v):
-    m=[]
-    for i in range(n):
-        line=[]
-        item_i=2*(i)
-        for j in range(n):
-            item_j=2*(j)
-            item=[item_i,item_j]
-            print(item)
+def MatrixElement(n, m, e, v):
+    if(n-m == 0):
+        return n*e
+    elif (n-m == 2):
+        return np.sqrt(m+1)*np.sqrt(m+2)*(-v)
+    elif (n-m == -2):
+        return np.sqrt(m)*np.sqrt(m-1)*(-v)
+    return 'Null'
 
-def CreateMatrix13(n,e,v):
-    m=[]
-    for i in range(n):
-        line=[]
-        item_i=2*i+1
-        for j in range(n):
-            item_j=2*j+1
-            item=[item_i,item_j]
-            print(item)
 
-CreateMatrix02(4,1,1)
-CreateMatrix13(4,1,1)
+def CreateMatrix02(n, e, v):
+    m = []
+    for i in range(n):
+        line = []
+        item_i = 2*(i)
+        for j in range(n):
+            item_j = 2*(j)
+            res = MatrixElement(item_i, item_j, 1, 1)
+            if(res != 'Null'):
+                print(item_i, item_j, res)
+
+
+def CreateMatrix13(n, e, v):
+    m = []
+    for i in range(n):
+        line = []
+        item_i = 2*i+1
+        for j in range(n):
+            item_j = 2*j+1
+            res = MatrixElement(item_i, item_j, 1, 1)
+            if(res != 'Null'):
+                print(item_i, item_j, res)
+
+
+CreateMatrix02(4, 1, 1)
+print('')
+CreateMatrix13(4, 1, 1)
