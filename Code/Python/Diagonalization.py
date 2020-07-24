41fea98ab622fcd9c036de0a36a521e9f207eb04
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-07-24 19:01:19 +0300

    adds output file with numerical values of lambda

--- Code/Python/Diagonalization.py
+++ Code/Python/Diagonalization.py
@@ -99,7 +99,8 @@ def LambdaEvolution(n, id, boson, nq):
 
 
 pathPrefix = '../../Reports/'
-
+foldername='LambdaPlots/'
+filename='../../Reports/solutions.txt'
 
 def omega(q):
     return np.sqrt(1.0 - 4.0*(np.power(q, 2)))
@@ -108,6 +109,8 @@ qOmegas=np.linspace(0,0.5,100)
 omegas=[]
 for x in qOmegas:
     omegas.append(omega(x))
+
+
 
 plotname=pathPrefix+'omegaCT.pdf'
 plt.plot(qOmegas,omegas,'-b')
@@ -118,7 +121,7 @@ def ShowSolutions(n, id):
     nqReduced=4
     nqM=100
     for i in range(n):
-        plotname=pathPrefix+'plot-id'+str(i+1)+'.pdf'
+        plotname=pathPrefix+foldername +'plot-id'+str(i+1)+'.pdf'
         fig, ax=plt.subplots()
         plt.xlabel('q')
         plt.ylabel(f'$\lambda_i$')
@@ -138,14 +141,22 @@ def ShowSolutions(n, id):
             print(f'Accurate| N={nqM}')
             print(qValues_M)
             print(xs_M)
+            f = open(filename, "w")
+            f.write(str(qValues_M))
+            f.write('\n')
+            f.write(str(xs_M))
+            f.close()
         ax.plot(qValues_Red, xs_Red, 'r-o', label=f'$n_d$={nqReduced}')
         ax.plot(qValues_M, xs_M, 'b-', label=f'$n_d$={nqM}')
         ax.legend(loc='best')
         plt.savefig(plotname, bbox_inches='tight')
         plt.close()
+    
 
 
-# ShowSolutions(10, 1)
+ShowSolutions(10, 1)
+
+print(np.arange(0,3,0.1))
 
 # plt.plot(qValues, LambdaEvolution(10, 1, 0), '-r')
 # plt.plot(qValues, LambdaEvolution(10, 2, 0), '-b')
