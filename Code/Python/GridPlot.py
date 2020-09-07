690f76506f55a2cc54f2ec993e7c92f1b206f789
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-09-07 17:50:28 +0300

    the plot method now takes special formatted data as arguments instead of locally generating data

--- Code/Python/GridPlot.py
+++ Code/Python/GridPlot.py
@@ -15,21 +15,26 @@ def give_array(n, param):
 def CreateGridPlot(n):
     params = [1, 2, 3, 4]
     array_dim = 10
+    data = GenerateData(2*n)
     # data = give_array(20, rd.choice(params))
-    x = give_array(array_dim, rd.choice(params))[0]
+    x = data[0]
+    # x = give_array(array_dim, rd.choice(params))[0]
 
     fig, ax = plt.subplots(n, n)
     fig.suptitle(f'$\lambda$ evolution with $q$-value', fontsize=14)
 
+    count = 0
     for i in range(n):
         for j in range(n):
-            y = give_array(array_dim, rd.choice(params))[1]
-            p = np.mean(y)
+            y = data[1][count]
+            # y = give_array(array_dim, rd.choice(params))[1]
+            p = round(np.mean(y),3)
             ax[i, j].plot(x, y, '-k')
             ax[i, j].plot(x, y, 'or')
             ax[i, j].text(0.15, 0.75, f'$\mu$={p}', horizontalalignment='center',
                           verticalalignment='center', transform=ax[i, j].transAxes)
             ax[i, j].set_title(f'$\lambda{(i+1,j+1)}$')
+            count = count+1
     for ax_id in ax.flat:
         ax_id.set(xlabel=f'$q$', ylabel=f'$\lambda$')
 
@@ -120,4 +125,19 @@ def CreateGridPlot(n):
     # wspace=0.5)
 
 
+def GenerateData(n):
+    params = [1, 2, 3, 4]
+    # xvalues
+    qvalues = np.arange(0, 3.1, 0.5)
+    # yvalues
+    yvalues = []
+    for id in range(n):
+        y = map(lambda x: rd.choice(params)*x*x, qvalues)
+        yvalues.append(list(y))
+    return [list(qvalues), yvalues]
+
+
+# print(f[0])
+# for id in range(len(f[1])):
+#     print(f[1][id])
 CreateGridPlot(2)
