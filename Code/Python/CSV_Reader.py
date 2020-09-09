3b81b9ba43bf526c2c87c3c441c5050fbb4e2672
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-09-09 09:24:27 +0300

    adds source for tutorial with external module usecasE

--- Code/Python/CSV_Reader.py
+++ Code/Python/CSV_Reader.py
@@ -1,6 +1,10 @@
 import csv as csv
 import numpy as np
 
+# used [this](https://www.digitalocean.com/community/tutorials/how-to-write-modules-in-python-3) tutorial for working with external modules as GridPlot and the EigenValues
+
+# import BosonEigenvalues as bos
+# import GridPlot as grid
 def GenerateFileNames(n, file_id):
     names = []
     for id in range(n):
@@ -11,13 +15,35 @@ def GenerateFileNames(n, file_id):
 
 filenames = GenerateFileNames(10, 'Lambda-idx')
 
-def ReadFile(filename):
-    with open(filename) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            line_count += 1
-        print(f'Processed {line_count} lines.')
 
-for file_id in filenames:
-    ReadFile(file_id)
+def ReadFile(filenames):
+    qvalues = []
+    lambdas = []
+
+    # first create q-array
+    # this can be done in a single loop, since the values of the q-parameter are the same across each file.
+    with open(filenames[0]) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            qvalues.append(round(float(row[0]), 3))
+
+    # create the lambda-array
+    # array of arrays, where each sub-array contains all the values of lambda_id, evaluated at certain q
+    count = 0
+    for filename in filenames:
+        lamda_local = []
+        with open(filename) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                lamda_local.append(float(row[1]))
+                # lamda_local.append(qvalues[count])
+        lambdas.append(lamda_local)
+        count = count+1
+    data = [qvalues, lambdas]
+    print(data)
+    return data
+
+
+ReadFile(filenames)
+
+# grid.CreateGridPlot(2,)
